359979d2236bbbda3dd386e905b19836b6c9a908
Paco Gallegos <pacog1994@gmail.com>
2018-04-17 20:01:51 -0400

    simple server update

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
-function sum (arr) {
-    return arr.reduce(function(a, b) { 
-      return a + b
-    }, 0)
-  }
-  
-  module.exports.sum = sum
+const calc = require('./calc')
+
+const numbersToAdd = [
+  3,
+  4,
+  10,
+  2
+]
+
+const result = calc.sum(numbersToAdd)
+console.log(`The result is: ${result}`)
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-require('./app/index')
+const fs = require('fs')
+
+console.log('start reading a file...')
+
+fs.readFile('file.md', 'utf-8', function (err, content) {
+  if (err) {
+    console.log('error happened during reading the file')
+    return console.log(err)
+  }
+
+  console.log(content)
+})
+
+console.log('end of the file')
+
+// require('./app/index')
+
+// const _ = require('lodash')
+
+// _.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
+// // → { 'a': 1, 'b': 2, 'c': 3 }
